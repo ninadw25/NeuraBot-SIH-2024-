474a77c953a6292c9b258fa5d5f3336f8613e763
PratyakshSaluja <pratyakshsirsa@gmail.com>
2024-08-28 14:17:17 +0530

    Update app.py

--- chat/app.py
+++ chat/app.py
@@ -258,8 +258,8 @@ def main():
         [
             "llama3-8b-8192",
             "gemma2-9b-it",
-            "llama3.1-70b-versatile",
-            "llama3.1-8b-instant",
+            "llama-3.1-70b-versatile",
+            "llama-3.1-8b-instant",
             "llama-guard-3-8b",
             "llama3-70b-8192",
             "llama3-groq-70b-8192-tool-use-preview",
