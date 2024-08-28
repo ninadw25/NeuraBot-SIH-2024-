f7039a629ff78cdc6e08a9d2d2b3ec71aa1093cb
Saksham Garg <saksham.garg14509@gmail.com>
2024-08-28 12:47:04 +0530

    Update index.js

--- nullPointers_2.0/index.js
+++ nullPointers_2.0/index.js
@@ -8,38 +8,7 @@ const { error } = require('console');
 const app = express();
 const PORT = 6969;
 
-// Connection
-// URI = "mongodb+srv://Saksham:ZkDY@203@null-pointers.vdsmm.mongodb.net/?retryWrites=true&w=majority&appName=null-pointers";
-
-// mongoose.connect(`${URI}`)
-//     .then(() => console.log("MongoDB connected successfully"))
-//     .catch((error) => console.log("Error connecting with MongoDB database", error));
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://Suhas:3786@null-pointers.vdsmm.mongodb.net/?retryWrites=true&w=majority&appName=null-pointers";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+mongodb+srv://<saksham>:1AGrEtT2wLj272T5@null-ptrs.2idnn.mongodb.net/?retryWrites=true&w=majority&appName=null-ptrs
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve('./views'));
