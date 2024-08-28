478e369701d48f2fbc1c2aa90bb30b50fe25336e
Saksham Garg <saksham.garg14509@gmail.com>
2024-08-28 12:50:45 +0530

    Update index.js

--- nullPointers_2.0/index.js
+++ nullPointers_2.0/index.js
@@ -8,7 +8,33 @@ const { error } = require('console');
 const app = express();
 const PORT = 6969;
 
-mongodb+srv://<saksham>:1AGrEtT2wLj272T5@null-ptrs.2idnn.mongodb.net/?retryWrites=true&w=majority&appName=null-ptrs
+// this is for connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<db_username>:<db_password>@null-ptrs.2idnn.mongodb.net/?retryWrites=true&w=majority&appName=null-ptrs";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve('./views'));
