43160f12f8a00ba58ce619b276e49034180f1b6a
SuhasKanwar <suhas.kanwar@gmail.com>
2024-09-04 17:30:11 +0530

    Update index.js
    Fresh

--- nullPointers_2.0/routes/index.js
+++ nullPointers_2.0/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const router = express.Router();
+
 const homeController = require('../controllers/homeController');
 const chatController = require('../controllers/chatController');
 
