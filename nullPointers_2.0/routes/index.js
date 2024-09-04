af6cb0a9b484175b54720142a73fd56365f9e363
SuhasKanwar <suhas.kanwar@gmail.com>
2024-09-04 21:19:41 +0530

    adding summarizer files

--- nullPointers_2.0/routes/index.js
+++ nullPointers_2.0/routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const homeController = require('../controllers/homeController');
 const chatController = require('../controllers/chatController');
+const summarizerController = require('../controllers/summarizerController');
 
 router.get('/', homeController.index);
 router.post('/chat', chatController.handleChat);
+router.get('/summarize', summarizerController.summarizer)
 
 module.exports = router;
