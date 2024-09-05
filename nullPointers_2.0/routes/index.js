e0188668f936579b51ee499d880d4a407c9a163f
PratyakshSaluja <pratyakshsirsa@gmail.com>
2024-09-05 22:12:47 +0530

    admin

--- nullPointers_2.0/routes/index.js
+++ nullPointers_2.0/routes/index.js
@@ -7,6 +7,7 @@ const chatController = require('../controllers/chatController');
 const { summarizerRender, summarizer } = require('../controllers/summarizerController');
 const homeController = require('../controllers/homeController');
 const loginController = require('../controllers/loginController');
+const adminController = require('../controllers/adminController')
 
 router.get('/', homeController.home);
 router.get('/login', loginController.login);
@@ -14,5 +15,5 @@ router.get('/chatbot', chatbotController.index);
 router.post('/chat', chatController.handleChat);
 router.get('/summarize', summarizerRender);
 router.post('/summarize', summarizer);
-
+router.get('/admin',adminController.admin)
 module.exports = router;
