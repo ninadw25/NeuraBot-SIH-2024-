1c27c0c53efe1f3e3511c409c650082411671e23
PratyakshSaluja <pratyakshsirsa@gmail.com>
2024-09-06 19:41:51 +0530

    Oauth againnn

--- nullPointers_2.0/index.js
+++ nullPointers_2.0/index.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"));
 
 app.use(session({
-  secret: 'your-secret-key',  // Replace with a secure key
+  secret: 'e33b92145a61635ff2992e8a4fc6a33711d4365bd7f6000276855498196aed93',  // Replace with a secure key
   resave: false,
   saveUninitialized: true
 }));
