04f56e2f5bef49d13953cbd0682eb69febf103d9
PratyakshSaluja <pratyakshsirsa@gmail.com>
2024-09-09 02:08:53 +0530

    asd

--- nullPointers_2.0/controllers/chatController.js
+++ nullPointers_2.0/controllers/chatController.js
@@ -7,7 +7,7 @@ exports.handleChat = async (req, res) => {
     const { prompt } = req.body;
     const context = fs.readFileSync(path.resolve('./assets/documents/document.txt'), 'utf-8');
     
-    const fullPrompt = `Greet the user if they do,Based on the following context, answer the question without providing information outside of it, if asked outside say no information avaialable:\n${context}\n\nQuestion: ${prompt}, no cuss words, if cuss words are sent by user say please be appropriate`;
+    const fullPrompt = `Based on the following context, answer the question no answering outside the context at all.:\n${context}\n\nQuestion: ${prompt}, no cuss words, if cuss words are sent by user say please be appropriate`;
     
     const chatCompletion = await getGroqChatCompletion(fullPrompt);
     const response = chatCompletion.choices[0]?.message?.content || "Sorry, I couldn't generate a response.";
